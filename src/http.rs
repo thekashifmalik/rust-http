df7f44028a749db726d255bcb2c78c2fddb6fb44
Kashif Malik <kashif610@gmail.com>
2014-09-11 04:59:29 -0700

    Add Request/Response objects

--- src/http.rs
+++ src/http.rs
@@ -14,8 +14,96 @@ struct Response {
     pub text: String
 }
 
+impl Response {
+	fn receive(mut stream: BufferedStream<TcpStream>) -> Result<Response, ()> {
+		let status_line = stream.read_line().unwrap();
+		let status_vector: Vec<&str> = status_line.as_slice().trim().splitn(2, ' ').collect();
 
-fn read_to_crlf<T: Stream>(stream: &mut BufferedStream<T>) -> IoResult<Vec<u8>> {
+		let header_bytes = read_to_headers(&mut stream).unwrap();
+
+		let header_string = match String::from_utf8(header_bytes) {
+			Ok(x) => x,
+			Err(_) => return Err(()),
+		};
+
+		let text_bytes = stream.read_to_end().unwrap();
+
+		Ok(Response {
+		    status_code: from_str(status_vector[1]).unwrap(),
+		    text: String::from_utf8(text_bytes).unwrap(),
+		})
+
+	}
+}
+
+// enum Method {
+//     Get,
+//     Post,
+//     Put,
+//     Delete,
+//     Patch,
+// }
+
+struct Request {
+    method: String,
+    host: String,
+    port: u16,
+    path: String,
+}
+
+impl Request {
+	fn new(method: &str, url_string: &str) -> Result<Request, ()> {
+		let url = match Url::parse(url_string) {
+			Ok(x) => x,
+			Err(_) => return Err(()),
+		};
+
+		let host = match url.domain() {
+			Some(x) => (x),
+			None => return Err(()),
+		};
+
+		let port = match url.port() {
+			Some(x) => x,
+			None => if url.scheme.as_slice() == "https" { 443 } else { 80 },
+		};
+
+		let path = match url.path() {
+			Some(x) => format!("/{}", x.connect("/")),
+			None => return Err(()),
+		};
+
+		Ok(Request {
+			method: method.to_string(),
+		    host: host.to_string(),
+		    port: port,
+			path: path.to_string(),
+		})
+	}
+
+	fn connect(&self) -> IoResult<BufferedStream<TcpStream>> {
+		Ok(BufferedStream::new(
+			TcpStream::connect(self.host.as_slice(), self.port).unwrap()
+		))
+	}
+
+	fn to_bytes(&self) -> Vec<u8> {
+		let string = format!(
+			"{} {} HTTP/1.0\n\n",
+			self.method,
+			self.path,
+		);
+		return string.into_bytes()
+	}
+
+	fn send(&self, stream: &mut BufferedStream<TcpStream>) {
+		stream.write(self.to_bytes().as_slice());
+		stream.flush();
+	}
+}
+
+
+fn read_to_crlf(stream: &mut BufferedStream<TcpStream>) -> IoResult<Vec<u8>> {
 	let mut read_upto = try!(stream.read_until('\r' as u8));
 
 	let next_char = try!(stream.read_byte());
@@ -30,7 +118,7 @@ fn read_to_crlf<T: Stream>(stream: &mut BufferedStream<T>) -> IoResult<Vec<u8>> 
 }
 
 
-fn read_to_headers<T: Stream>(stream: &mut BufferedStream<T>) -> IoResult<Vec<u8>> {
+fn read_to_headers(stream: &mut BufferedStream<TcpStream>) -> IoResult<Vec<u8>> {
 	let mut read_upto = try!(read_to_crlf(stream));
 
 	if read_upto.as_slice() == b"\r\n" {
@@ -50,54 +138,15 @@ fn read_to_headers<T: Stream>(stream: &mut BufferedStream<T>) -> IoResult<Vec<u8
 }
 
 pub fn get(url_string: &str) -> Result<Response, ()> {
-	// Get address
-	let url = match Url::parse(url_string) {
-		Ok(x) => x,
-		Err(_) => return Err(()),
-	};
+	
+	let request = Request::new("GET", url_string).unwrap();
 
-	let host = match url.domain() {
-    	Some(x) => (x),
-    	None => return Err(()),
-	};
+	let mut stream = request.connect().unwrap();
 
-	let port = match url.port() {
-		Some(x) => x,
-		None => if url.scheme.as_slice() == "https" { 443 } else { 80 },
-	};
+	request.send(&mut stream);
 
-	let path = match url.path() {
-		Some(x) => format!("/{}", x.connect("/")),
-    	None => return Err(()),
-	};
+	// stream.write(request.to_bytes().as_slice());
+	// stream.flush();
 
-	let mut stream = match TcpStream::connect(host, port) {
-		Ok(x) => BufferedStream::new(x),
-		Err(_) => return Err(()),
-	};
-
-	let status_line_to_send = format!("GET {} HTTP/1.0\n\n", path);
-	println!("{}", status_line_to_send);
-	stream.write(status_line_to_send.as_bytes());
-	stream.flush();
-
-	let status_line = stream.read_line().unwrap();
-	let status_vector: Vec<&str> = status_line.as_slice().trim().splitn(2, ' ').collect();
-
-	let header_bytes = read_to_headers(&mut stream).unwrap();
-
-	let header_string = match String::from_utf8(header_bytes) {
-		Ok(x) => x,
-		Err(_) => return Err(()),
-	};
-
-	println!("{}", header_string);
-	println!("{}", status_vector);
-
-	let text_bytes = stream.read_to_end().unwrap();	
-
-	Ok(Response {
-	    status_code: from_str(status_vector[1]).unwrap(),
-	    text: String::from_utf8(text_bytes).unwrap(),
-	})
+	return Response::receive(stream);
 }
