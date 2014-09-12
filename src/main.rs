e492db82c8259f49d1666917e24ce1838f7a4f32
Kashif Malik <kashif610@gmail.com>
2014-09-12 16:28:42 -0700

    update example

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod http;
 
 fn main() {
 
-	let response = http::get(
-        "http://www.google.com",
-    ).ok().expect("There was a problem making the request");
+	let response = match http::get("http://www.google.com") {
+        Ok(resp) => resp,
+        Err(error) => fail!("There was a problem making the request"),
+    };
 
 	println!("status code: {}", response.status_code);
 	println!("body length: {}", response.text.len());
